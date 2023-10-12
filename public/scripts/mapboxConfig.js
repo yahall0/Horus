1ac4a08d21f4882db4665e68e50589879d099f3b
ShaunikGupta23 <guptashaunik@gmail.com>
2023-10-12 12:42:22 +0530

    test

--- public/scripts/mapboxConfig.js
+++ public/scripts/mapboxConfig.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = mapboxTokenREMOVE;
+mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
