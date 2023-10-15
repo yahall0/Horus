88883ef5baec529dc032471c5c5b8baea3abee2c
ShaunikGupta23 <guptashaunik@gmail.com>
2023-10-15 15:58:22 +0530

    Changes in css

--- public/scripts/mapboxConfig.js
+++ public/scripts/mapboxConfig.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = mapboxToken;
+mapboxgl.accessToken = mapboxTokenREMOVE;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
